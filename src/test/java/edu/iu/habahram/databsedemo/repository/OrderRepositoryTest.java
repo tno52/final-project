23d4d7de27a74e5875e865a849b6da3ead73aea1
hbahramian <habahram@iu.edu>
2024-04-24 14:52:07 -0400

    .

---
+++ src/test/java/edu/iu/habahram/databsedemo/repository/OrderRepositoryTest.java
@@ -0,0 +1,12 @@
+package edu.iu.habahram.databsedemo.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepositoryTest {
+
+    @Test
+    void findAllByCustomerUserName() {
+    }
+}
