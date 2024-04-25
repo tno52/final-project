8e55ec94b59cd2bf94c2a411338aebff2fcbe722
hbahramian <habahram@iu.edu>
2024-04-25 00:09:24 -0400

    .

--- src/main/java/edu/iu/habahram/databsedemo/repository/OrderRepository.java
+++ src/main/java/edu/iu/habahram/databsedemo/repository/OrderRepository.java
@@ -3,12 +3,14 @@ package edu.iu.habahram.databsedemo.repository;
 import edu.iu.habahram.databsedemo.model.Customer;
 import edu.iu.habahram.databsedemo.model.Order;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface OrderRepository
-        extends CrudRepository<Order, Integer> {
+        extends CrudRepository<Order, Integer>
+, QueryByExampleExecutor<Order> {
     List<Order> findAllByCustomerUserName(String customerUserName);
 }
